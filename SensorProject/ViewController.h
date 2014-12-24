cd058124e56ecd5375d224bf0ffec8b5337582b9
Ali Ugur Aker <aliuguraker91@gmail.com>
2014-12-24 22:46:25 +0200

    Initial Commit

---
+++ SensorProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SensorProject
+//
+//  Created by Ali Ugur Aker on 24.12.2014.
+//  Copyright (c) 2014 Ali Ugur Aker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SensorProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SensorProject
+//
+//  Created by Ali Ugur Aker on 24.12.2014.
+//  Copyright (c) 2014 Ali Ugur Aker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
